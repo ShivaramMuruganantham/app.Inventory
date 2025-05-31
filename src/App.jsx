17cd11c8401513438f738411385bd63e9af68cbb
Shivaram <shivaram1216@gmail.com>
2025-05-31 17:38:59 +0530

    Creating route for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ReactDom from 'react-dom'
+import { Route, Routes, BrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Onboarding from './pages/Onboarding'
+import AdminLogin from './pages/AdminLogin'
+import UserLogin from './pages/UserLogin'
+import Dashboard from './pages/Dashboard'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="mx-auto max-w-[500px] min-h-screen shadow">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Onboarding />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/user-login" element={<UserLogin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
