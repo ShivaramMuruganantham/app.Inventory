a4f421763d6747b26185f4f6747166c35b4bb99e
Shivaram <shivaram1216@gmail.com>
2025-07-02 10:38:41 +0530

    Api slice config file

---
+++ src/utils/slices/apiConfigSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const apiConfigSlice = createSlice({
+    name: "apiCOnfigSlice",
+    initialState: {
+        baseURL : 'http://127.0.0.1:8000/api',
+    },
+
+    reducers: {
+        setBaseURL : (state, action) => {
+            state.baseURL = action.payload
+        }
+    }, 
+})
+
+export const { setBaseURL } = apiConfigSlice.actions;
+export default apiConfigSlice.reducer;
