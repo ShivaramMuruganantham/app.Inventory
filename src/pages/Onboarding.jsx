ec1a209121e97215a249a259d376e364e9e9b204
Shivaram <shivaram1216@gmail.com>
2025-07-02 10:32:32 +0530

    update onboarding page

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom"
 
+import Input from "../components/form/input"
+
 function Onboarding() {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="min-h-screen">
             <div>
-                <Link to="/admin-login" className="text-2xl font-bold text-center mb-4 p-2 border w-60 rounded">Admin Dashboard</Link>
-                <Link to="/user-login" className="text-2xl font-bold text-center mb-4 p-2 border w-60 rounded">User Dashboard</Link>
+                <p className="text-2xl font-bold text-center mb-4 p-2 border w-52 rounded">Welcome to App</p>
+                <Link to="/shop-register" className="text-2xl font-bold mb-4 p-2 border w-52 rounded">New to the app</Link>
+                <Link to="/user-login" className="text-2xl font-bold mb-4 p-2 border w-52 rounded">Already a user</Link>
+            </div>
+            {/* <div>
+                <Link to="/admin-login" className="text-2xl font-bold text-center mb-4 p-2 border w-52 rounded">Admin Dashboard</Link>
+                <Link to="/user-login" className="text-2xl font-bold text-center mb-4 p-2 border w-52 rounded">User Dashboard</Link>
             </div>
             <div>
                 <p></p>
-            </div>
+            </div> */}
         </div>
     )
 }
