9b80e4cb484f0bb24c4d999b0e035cdf69240a4e
Shivaram <shivaram1216@gmail.com>
2025-07-02 10:37:17 +0530

    Update validation form

--- src/utils/mixins/FormValidte.js
+++ src/utils/mixins/FormValidte.js
@@ -1,4 +1,14 @@
 export const FormValidate = {
+    name : [
+        (v) => !!v || "Name is required",
+        (v) => v && v.length >= 3 && v.length <= 20 || "Name must be more than 3 characters and less than 20 characters",
+        (v) => /^[a-zA-Z\s]+$/.test(v) || "Name must contain only letters and spaces"
+    ],
+    phone:[
+        (v) => !!v || "Phone number is required",
+        (v) => v && v.length === 10 || "Phone number must be 10 digits",
+        (v) => /^[0-9]+$/.test(v) || "Phone number must contain only digits"
+    ],
     email: [
         (v) => !!v || "E-mail is required",
         (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
@@ -9,11 +19,23 @@ export const FormValidate = {
         (v) => /[A-Z]/.test(v) || "Password must contain at least one uppercase letter",
         (v) => /[a-z]/.test(v) || "Password must contain at least one lowercase letter",
         (v) => /[0-9]/.test(v) || "Password must contain at least one number",
-        (v) => /[!@#$%^&*]/.test(v) || "Password must contain at least one special character",
+        (v) => /[!@#$%^&_*]/.test(v) || "Password must contain at least one special character",
     ],
 };
 
-export function FormValidateField(fieldName, value) {
+export function LoginValidate(fieldName, value) {
+    const rules = FormValidate[fieldName];
+    if (!rules) return "";
+
+    for (let rule of rules) {
+        const error = rule(value);
+        if (error !== true) return error;
+    }
+
+    return "";
+}
+
+export function RegisterValidate(fieldName, value) {
     const rules = FormValidate[fieldName];
     if (!rules) return "";
 
