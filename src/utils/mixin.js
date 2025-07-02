c1b0d40a340c4dad0c887b319fcc50a4f7cd30d7
Shivaram <shivaram1216@gmail.com>
2025-07-02 10:39:29 +0530

    Create mixins and optimise the fetch method

---
+++ src/utils/mixin.js
@@ -0,0 +1,44 @@
+import { useSelector } from "react-redux";
+export function useFetch() {
+    
+    const baseURL = useSelector(state => state.apiConfig.baseURL);
+
+    const token = JSON.parse(localStorage.getItem("user_data"));
+    // console.log(token.token);
+    function request(method = 'GET', url, body = {}, successMsg, errorMsg) {
+    
+        if(!url.includes("http")) {
+            url = baseURL + url;
+        }
+    
+        const requestOptions = {
+            method,
+            headers: {
+                'Authorization': `Bearer ${token.token}`,
+            }
+        }
+    
+        if(method === 'POST' || method === 'PUT') {
+            requestOptions.headers['Accept'] = 'application/json';
+            if(body instanceof FormData) {
+                requestOptions.body = body;
+            } else {
+                requestOptions.headers['Content-Type'] = 'application/json';
+                requestOptions.body = JSON.stringify(body);
+            }
+        }
+    
+        fetch(url, requestOptions)
+        .then(resp => resp.json())
+        .then(resp => {
+            // console.log(resp);
+            successMsg(resp);
+        })
+        .catch(err => {
+            if(errorMsg) errorMsg(err);
+            else console.log(err);
+        });
+    }
+
+    return request;
+}
