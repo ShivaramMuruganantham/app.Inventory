f8dc5f8560525da8f252cb82a11ee0e92ddb4db8
Shivaram <shivaram1216@gmail.com>
2025-07-02 10:28:49 +0530

    Update route file path

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './utils/store.js'
 
 import './index.css'
-import App from './App.jsx'
+import App from './Routes/AppRoutes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
