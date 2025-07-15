d3024e50b2e86e899b9fe1a84ed66e7978ccd05a
Shivaram <shivaram1216@gmail.com>
2025-07-15 09:57:32 +0530

    Update product componenet for getting data form backend data

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,85 +1,150 @@
-import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
-import { useState } from "react"
+import { Bar, Legend, BarChart, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
+import { useEffect, useState } from "react"
+import { useFetch } from "../utils/mixin"
+ 
+import AddStockModal from "../components/modal/addStock";
 
 function product() {
 
     const [ProductSale, setProductSale] = useState([]);
-    const [ShowData, setShowData] = useState("");
+    const [ShowData, setShowData] = useState([]);
+    const [Years, setYears] = useState([]);
+    const [category, setCategory] = useState([]);
 
-    function getData() {
-        const response = fetch("");
-        const data = response.json();
-        console.log(data.data);
-        setProductSale(data.data);
-        setShowData(data.data);
+    const currentYear = new Date().getFullYear();
+    const [selectedYear, setSelectedYear] = useState(currentYear);
+
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [updateStock, setUpdateStock] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+
+    const _fetch = useFetch();
+
+    useEffect(() => {
+        const response = _fetch('GET', '/analysis/product/years', {}, (resp) => {
+            if (resp.status) {
+                setYears(resp.years);
+
+                if (resp.years.includes(currentYear)) {
+                    setSelectedYear(currentYear);
+                }
+            }
+        })
+
+        const response2 = _fetch('GET', '/shop/product/category', {}, (resp) => {
+            if (resp.status) {
+                // console.log("Categories", resp.categories);
+                setCategory(resp.categories);
+            }
+        })
+    }, []);
+
+    useEffect(() => {
+        if (selectedYear) {
+            const response = _fetch('GET', `/analysis/product/graph/${selectedYear}`, {}, (resp) => {
+                if (resp.status) {
+                    // console.log("Graph Data", resp.graph);
+                    setProductSale(resp.graph);
+
+                }
+            })
+        }
+    }, [selectedYear]);
+
+    function fetchProducts() {
+        const response = _fetch('GET', '/shop/product', {}, (resp) => {
+            if (resp.status) {
+                setShowData(resp.products);
+            }
+        })
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    function getColor(index) {
+        const colors = [
+            "#0b00df", "#00ee59", "#ffa800", "#ff4400", "#00cbfb",
+            "#78f400", "#c5f402", "#ff0c00", "#8724ff", "#ff3525",
+            "#02e15d", "#0bcece", "#0722bb", "#f2d49e", "#c90076"
+        ];
+        return colors[index % colors.length];
     }
 
     return (
-        <div>
+        <div className="relative">
             <div className="py-5 bg-white shadow">
                 <div className="flex justify-between px-5">
                     <p className="text-lg font-medium">Product</p>
-                    <select name="" id="" className="border p-1 rounded text-sm">
-                        <option value="">Months</option>
-                        <option value="">Jan</option>
-                        <option value="">Feb</option>
+                    <select value={selectedYear} onChange={(e) => setSelectedYear(Number(e.target.value))} className="border p-1 rounded text-sm outline-none">
+                        <option value="">Select year</option>
+                        {Years.map((year, index) => (
+                            <option value={year} key={index}>{year}</option>
+                        ))}
                     </select>
                 </div>
 
                 <div className="pt-7 pr-2">
                     <ResponsiveContainer width="100%" height={250}>
-                        <AreaChart width={500} height={400} data={ProductSale}>
+                        <BarChart width={500} height={400} data={ProductSale}>
                             <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
+                            <XAxis dataKey="month" />
                             <YAxis />
                             <Tooltip />
-                            <Area type="monotone" dataKey="pv"></Area>
-                        </AreaChart>
+                            {/* <Legend /> */} for hide the category names
+                            {category.map((item, index) => (
+                                <Bar key={item.category_name} dataKey={item.category_name} stackId="a" fill={getColor(index)} />
+                            ))}
+                        </BarChart>
                     </ResponsiveContainer>
                 </div>
             </div>
 
-            <div className="py-3 mt-2 bg-white shadow">
+            <div className=" py-3 mt-2 bg-white shadow ">
                 <div className="flex justify-between px-5">
                     <p className="text-lg font-medium">Products</p>
                     <div className="flex gap-5">
-                        <div className="border rounded">
-                            <i className="fa-solid fa-calendar-days p-1"></i>
-                            <select name="" id="">
-                                <option value="">Months</option>
-                                <option value="">Jan</option>
-                                <option value="">Feb</option>
-                            </select>
-                        </div>
-
-                        <select name="" id="" className="border rounded">
+                        <select name="" id="" className="border rounded outline-none" onChange={(e) => setSelectedCategory()}>
                             <option value="">Category</option>
-                            <option value="">1</option>
-                            <option value="">2</option>
+                            {category.map((item, index) => (
+                                <option value={item.id} key={index}>{item.category_name}</option>
+                            ))}
                         </select>
                     </div>
                 </div>
                 <div>
-                    <div className="flex justify-between pt-5 px-5">
-                        <p>All</p>
-                        <p>Food</p>
-                        <p>Snacks</p>
-                        <p>Drinks</p>
+                    <div>
+                        <p>{selectedCategory}</p>
                     </div>
-                    {/* {ShowData.map((item, index) => {
-                        <div key={index} className="flex gap-2 px-5 py-5 m-1 rounded bg-white">
-                            <img src={item.image} alt=""  className="border h-14 w-20"/>
-                            <div className="flex justify-between gap-x-20">
-                                <div>
-                                    <p>{item.name}</p>
-                                    <p>{item.price}</p>
-                                </div>
-                                <div>
-                                    <button className="cursor-pointer">stock : {item.stockCount}</button>  
-                                </div>
+                    {ShowData.map((product, index) => (
+                        <div className="flex justify-around border p-3 m-5 bg-white" key={index}>
+                            <img src="../assets/images/Moong.png" alt="" className="w-20 border"/>
+                            <div>
+                                <p>{product.name} <span>({ product.quantity })</span></p>
+                                <p>{ product.price }</p>
+                                <p>{ product.brand } </p>
+                            </div>
+                            <div>
+                                <p>{ product.inventory[0].stock_qty }</p>
+                                <button className="border px-3 py-1 text-sm cursor-pointer" onClick={() => {
+                                    setSelectedProduct(product);
+                                    setUpdateStock(true);
+                                }}>Add +</button>
                             </div>
                         </div>
-                    })} */}
+                    ))}
+                </div>
+                <div className="">
+                    {updateStock && (
+                        <AddStockModal
+                            product={selectedProduct}
+                            onClose={() => {
+                                setUpdateStock(false);
+                                fetchProducts();
+                            }}
+                        />
+                    )}
                 </div>
             </div>
         </div>
