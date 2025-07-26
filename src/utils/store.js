5b483428879281a1906bcb2cea6970a39560e0f2
Shivaram <shivaram1216@gmail.com>
2025-07-26 09:15:52 +0530

    add cart slice in store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import apiConfigReducer from "./slices/apiConfigSlice";
+import cartReducer from "./slices/cartSlice";
 
 export const useStore = configureStore({ 
     devTools: true,
     reducer: {
         apiConfig : apiConfigReducer,
+        cart : cartReducer
     } 
 });
 
