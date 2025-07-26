e8e13f438dfd26d2e4ad74890122fcc1dee399a9
Shivaram <shivaram1216@gmail.com>
2025-07-26 09:04:23 +0530

    small design fix

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -4,11 +4,14 @@ import Input from "../components/form/input"
 
 function Onboarding() {
     return (
-        <div className="min-h-screen">
-            <div>
-                <p className="text-2xl font-bold text-center mb-4 p-2 border w-52 rounded">Welcome to App</p>
-                <Link to="/shop-register" className="text-2xl font-bold mb-4 p-2 border w-52 rounded">New to the app</Link>
-                <Link to="/user-login" className="text-2xl font-bold mb-4 p-2 border w-52 rounded">Already a user</Link>
+        <div className="min-h-screen bg-gray-100 flex justify-center items-center px-5">
+            <div className="bg-white p-8 rounded-xl shadow text-center">
+                <p className="text-2xl font-medium mb-4">Welcome to App</p>
+                <p className="text-gray-500 mb-6">Get started by creating a new account or logging in.</p>
+                <div className="flex justify-center gap-6 mt-4">
+                    <Link to="/shop-register" className="text-sm bg-gray-200 py-2 px-5 rounded-full">New to the app</Link>
+                    <Link to="/user-login" className="text-sm bg-gray-200 py-2 px-5 rounded-full">Already a user</Link>
+                </div>
             </div>
             {/* <div>
                 <Link to="/admin-login" className="text-2xl font-bold text-center mb-4 p-2 border w-52 rounded">Admin Dashboard</Link>
